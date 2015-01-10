974de13021e8b9a409d02d3ef9c3fabe01015035
faust1002 <faust1002@gmail.com>
2015-01-10 14:54:33 +0100

    added uart

--- verilog/eth_debug.v
+++ verilog/eth_debug.v
@@ -1,2 +1,9 @@
-module eth_debug;
+module eth_debug(
+    input wire clk, reset,
+    output wire uart_tx,
+    output wire uart_empty, uart_full);
+
+    uart uart_unit (.clk(clk), .reset(reset), .tx(uart_tx), .wr_uart(1'b1),
+                    .w_data(8'h30), .tx_empty(uart_empty), .tx_full(uart_full));
+
 endmodule
