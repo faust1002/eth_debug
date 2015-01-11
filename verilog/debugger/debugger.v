5e4701e938237bbec89c262076903dcc773385b2
faust1002 <faust1002@gmail.com>
2015-01-11 13:15:59 +0100

    added bin2ascii

--- verilog/debugger/debugger.v
+++ verilog/debugger/debugger.v
@@ -1,15 +1,15 @@
 module debugger(
     input wire clk, reset,
-    input wire [7:0] w,
+    input wire [3:0] w,
     output wire trigger,
-    output wire [7:0] data
+    output wire [3:0] data
 );
 
-    reg [7:0] data_reg, data_next;
+    reg [3:0] data_reg, data_next;
 
     always @(posedge clk, posedge reset)
         if (reset)
-            data_reg <= 8'h0;
+            data_reg <= 4'h0;
         else
             data_reg <= data_next;
 
--- verilog/eth_debug.v
+++ verilog/eth_debug.v
@@ -1,17 +1,20 @@
 module eth_debug(
     input wire clk, reset,
-    input wire [7:0] sw,
+    input wire [3:0] sw,
     output wire uart_tx,
     output wire uart_empty, uart_full);
 
-    wire [7:0] trigger_data;
+    wire [3:0] trigger_data;
+    wire [7:0] ascii_data;
     wire trigger;
+
+    bin2ascii bin_utils (.bin(trigger_data), .ascii(ascii_data));
 
     debugger debugger_unit (.clk(clk), .reset(reset),
                           .w(sw), .trigger(trigger),
                           .data(trigger_data));
 
     uart uart_unit (.clk(clk), .reset(reset), .tx(uart_tx), .wr_uart(trigger),
-                    .w_data(trigger_data), .tx_empty(uart_empty), .tx_full(uart_full));
+                    .w_data(ascii_data), .tx_empty(uart_empty), .tx_full(uart_full));
 
 endmodule
