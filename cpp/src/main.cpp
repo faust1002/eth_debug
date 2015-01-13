ac3542b1fe7c53addfebf6a745b7c093707b0673
faust1002 <faust1002@gmail.com>
2015-01-13 21:16:43 +0100

    explicit type replaced by auto

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int main()
 {
-    std::shared_ptr<application::Application> l_app = application::createApplication();
-    std::shared_ptr<debugger::Link> l_link = std::make_shared<debugger::Link>();
+    auto l_app = application::createApplication();
+    auto l_link = std::make_shared<debugger::Link>();
     l_link->addObserver(l_app);
     std::thread l_appThread {[&]{l_app->run();}};
     std::thread l_linkThread {[&]{l_link->run();}};
